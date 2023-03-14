00e64ea6695cdd077c61dbefcf760e6aa90faef8
Sameed Al Islam <sameedsir@gmail.com>
2023-03-14 21:04:45 +0600

    This is my first MVC Structure with Express project. There was no failure to build this API project. Here I used 2 html pages which are Users & Products. You can add your product via form submition. Give a star to it. Thanks for visiting my current projcect. SAMEED SIR.

---
+++ models/products.model.js
@@ -0,0 +1,13 @@
+const products = [
+    {
+      name: "Hp Elitebook G5",
+      price: 1299,
+    },
+    {
+        name: "Vivo Y21",
+        price: 21,
+    },
+  ];
+  
+  module.exports = products;
+  
---
+++ models/users.model.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    name: "Sameed Al Islam",
+    age: 19,
+  },
+  {
+    name: "Anna Khatun",
+    age: 17,
+  },
+];
+
+module.exports = users;
